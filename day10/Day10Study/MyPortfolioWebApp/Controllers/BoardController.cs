c0d8c65182e047e490e3c56821f2f7cf8515d550
Woo Dong Kwan <ehdrhks0509@naver.com>
2025-06-11 15:41:47 +0900

    좋ㅇ아
    좋습니다.1

--- day10/Day10Study/MyPortfolioWebApp/Controllers/BoardController.cs
+++ day10/Day10Study/MyPortfolioWebApp/Controllers/BoardController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization; // 추가
 using MyPortfolioWebApp.Models;
 
 namespace MyPortfolioWebApp.Controllers
@@ -17,41 +18,30 @@ namespace MyPortfolioWebApp.Controllers
         public async Task<IActionResult> Index(int page = 1, string search = "")
         {
             ViewData["Title"] = "게시판 목록";
-
-            // 검색 조건 적용
             var query = _context.Board.AsQueryable();
             if (!string.IsNullOrEmpty(search))
             {
                 query = query.Where(b => EF.Functions.Like(b.Title, $"%{search}%"));
             }
-
-            // 전체 게시글 수
             var totalCount = await query.CountAsync();
-            var countList = 10; // 한 페이지에 보여줄 게시글 수
+            var countList = 10;
             var totalPage = totalCount / countList;
             if (totalCount % countList > 0) totalPage++;
             if (totalPage < page) page = totalPage;
-
-            // 페이징 네비게이션 계산
-            var countPage = 10; // 페이지 네비에 보여줄 최대 페이지 수
+            var countPage = 10;
             var startPage = ((page - 1) / countPage) * countPage + 1;
             var endPage = startPage + countPage - 1;
             if (totalPage < endPage) endPage = totalPage;
-
-            // 실제 데이터 가져오기 (최신순)
             var boards = await query
                 .OrderByDescending(b => b.PostDate)
                 .Skip((page - 1) * countList)
                 .Take(countList)
                 .ToListAsync();
-
-            // ViewBag에 페이징 정보 전달
             ViewBag.StartPage = startPage;
             ViewBag.EndPage = endPage;
             ViewBag.Page = page;
             ViewBag.TotalPage = totalPage;
             ViewBag.Search = search;
-
             return View(boards);
         }
 
@@ -65,7 +55,6 @@ namespace MyPortfolioWebApp.Controllers
             if (board == null)
                 return NotFound();
 
-            // 조회수 증가 (null 방지)
             board.ReadCount = (board.ReadCount ?? 0) + 1;
             _context.Board.Update(board);
             await _context.SaveChangesAsync();
@@ -74,6 +63,7 @@ namespace MyPortfolioWebApp.Controllers
         }
 
         // GET: Board/Create
+        [Authorize]
         public IActionResult Create()
         {
             var board = new Board
@@ -88,6 +78,7 @@ namespace MyPortfolioWebApp.Controllers
         // POST: Board/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Create([Bind("Email,Writer,Title,Contents")] Board board)
         {
             if (ModelState.IsValid)
@@ -105,6 +96,7 @@ namespace MyPortfolioWebApp.Controllers
         }
 
         // GET: Board/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -120,6 +112,7 @@ namespace MyPortfolioWebApp.Controllers
         // POST: Board/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Email,Writer,Title,Contents")] Board board)
         {
             if (id != board.Id)
@@ -137,7 +130,6 @@ namespace MyPortfolioWebApp.Controllers
                     existingBoard.Writer = board.Writer;
                     existingBoard.Title = board.Title;
                     existingBoard.Contents = board.Contents;
-                    // PostDate, ReadCount는 원본 유지
 
                     await _context.SaveChangesAsync();
                     TempData["success"] = "게시글이 수정되었습니다!";
@@ -155,6 +147,7 @@ namespace MyPortfolioWebApp.Controllers
         }
 
         // GET: Board/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -170,6 +163,7 @@ namespace MyPortfolioWebApp.Controllers
         // POST: Board/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var board = await _context.Board.FindAsync(id);
